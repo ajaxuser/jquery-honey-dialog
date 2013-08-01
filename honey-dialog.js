b7f525964e6e08fe5a076afb39ad2e1df1f47235
ajaxuser <666zhen@163.com>
2013-08-01 12:33:53 +0800

    dialog plugin

--- honey-dialog.js
+++ honey-dialog.js
@@ -17,11 +17,24 @@ jQuery(function($){
     Honey.prototype = $.fn = $.prototype = jQuery.fn = jQuery.prototype;
     
     //dialog
-    Honey.prototype.dialog = function(str) {
-        var shadow_height = document.documentElement.scrollHeight+'px';
-        var dialog_top = document.documentElement.scrollTop+150+'px';
+    Honey.prototype.dialog = function(msg) {
+        if(window.navigator.userAgent.indexOf('Chrome') !== -1) {
+            var shadow_height = document.body.scrollHeight+'px';
+            var dialog_top = document.body.scrollTop+150+'px';
+        } else {
+            var shadow_height = document.documentElement.scrollHeight+'px';
+            var dialog_top = document.documentElement.scrollTop+150+'px';
+        }
+        
         $(this).append('<div class="shadow" style="height:'+shadow_height+';"></div>');
-        $(this).append('<div class="dialog" style="top:'+dialog_top+';">'+str+'<div class="dialog_close"></div></div>');
+        
+        var dialog = 
+        '<div class="dialog" style="top:'+dialog_top+';">'+
+        '<div class="notice">'+'<div class="msg">'+msg+'</div>'+'<div class="buttons"><a class="button confirm_button">确定</a>&#160;<a class="button cancel_button">取消</a></div>'+'</div>'+
+        '<div class="dialog_close"></div>'+
+        '</div>';
+        
+        $(this).append(dialog);
         
         //鼠标拖动弹层
         $('div.dialog').mouseover(function(e){
@@ -56,7 +69,7 @@ jQuery(function($){
         });
         
         //点击删除按钮关闭弹层
-        $('div.dialog_close').click(function(){
+        $('div.dialog_close, a.cancel_button').click(function(){
             $('div.dialog').remove();
             $('div.shadow').remove();
         });
